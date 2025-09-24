5370ffac181b0cc477520741fedd1332c8d8be49
Victor Carmona <victorcarmona9035.com>
2025-09-24 12:35:12 +0200

    Cache de resultados

--- src/app/country/services/country.ts
+++ src/app/country/services/country.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountryModel } from '../interfaces/rest-countries.interface';
-import { catchError, delay, map, Observable, of, throwError } from 'rxjs';
+import { catchError, delay, map, Observable, of, tap, throwError } from 'rxjs';
 import type { CountryI } from '../interfaces/country.interface';
 import { CountryMapper } from '../components/mappers/country.mapper';
 
@@ -13,7 +13,8 @@ const API_URL = 'https://restcountries.com/v3.1';
 })
 export class Country {
 
-  private http = inject(HttpClient)
+  readonly #http = inject(HttpClient)
+  readonly #queryCacheCapital = new Map<string,CountryI[]>();
 
   /**
    *
@@ -22,28 +23,29 @@ export class Country {
    */
   searchByCapital(query: string): Observable<CountryI[]> {
     query = query.toLowerCase();
-    console.log(`Emitiendo el ${query}`)
 
-    return of([]);
+    if(this.#queryCacheCapital.has(query)){
+      return of(this.#queryCacheCapital.get(query) ?? []);
+    }
 
-    // return this.http.get<RESTCountryModel.RESTCountry[]>(`${API_URL}/capital/${query}`)
-    //   .pipe(
-    //     map((resp) => CountryMapper.mapRestCountryToCountryArray(resp)),
-    //     delay(3000),
-    //     catchError(error => {
-    //       console.log('Error fetching', error)
+    return this.#http.get<RESTCountryModel.RESTCountry[]>(`${API_URL}/capital/${query}`)
+      .pipe(
+        map((resp) => CountryMapper.mapRestCountryToCountryArray(resp)),
+        tap(countries => this.#queryCacheCapital.set(query,countries)),
+        catchError(error => {
+          console.log('Error fetching', error)
 
-    //       return throwError(
-    //         ()=> new Error(`No se pudo obtener paises con ese query ${query}`)
-    //       )
-    //     })
-    //   );
+          return throwError(
+            ()=> new Error(`No se pudo obtener paises con ese query ${query}`)
+          )
+        })
+      );
   }
 
   searchByCapitalByAlphaCode(code: string){
     const url = `${API_URL}/alpha/${code}`
 
-    return this.http.get<RESTCountryModel.RESTCountry[]>(url)
+    return this.#http.get<RESTCountryModel.RESTCountry[]>(url)
       .pipe(
         map((resp) => CountryMapper.mapRestCountryToCountryArray(resp)),
         map(countries=>countries.at(0)),
